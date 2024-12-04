17c6479ff34e60904105052318f6301cc98683a8
Frankam487 <fkamgang06@gmail.com>
2024-12-04 21:52:55 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: [
+    "./index.html",          // Inclut ton fichier HTML principal
+    "./src/**/*.{js,ts,jsx,tsx}", // Inclut tes fichiers React/JS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
