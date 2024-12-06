a17cdae57ce93316d7dc5658f1c4d7ba9cd78bff
Frankam487 <fkamgang06@gmail.com>
2024-12-06 23:44:45 +0200

    about file cleaned

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className='about'>
       <Navigations/>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis possimus ipsa sed aliquid molestiae. Repellat quae nemo mollitia adipisci tempore numquam iusto possimus ipsa fugit nostrum in libero iure, fuga, porro cupiditate deleniti voluptas! Consectetur reprehenderit dolorem distinctio voluptatibus doloremque perspiciatis architecto debitis excepturi, quae quisquam accusantium impedit numquam. Quas, magnam distinctio laudantium quia praesentium atque asperiores neque ea iure, temporibus iusto optio explicabo. Numquam odit porro quis dolor, expedita eligendi omnis magnam, at iusto commodi ipsam suscipit alias. Velit fugit blanditiis quasi aspernatur in officia reprehenderit porro tempore pariatur praesentium adipisci, exercitationem, sunt excepturi enim tenetur aperiam est laborum. Aspernatur quisquam blanditiis debitis eos exercitationem aut, recusandae saepe ea animi sint veritatis unde rerum nemo perspiciatis accusantium. Omnis vitae illo voluptatum minima quo officia. Quae corrupti molestiae consectetur asperiores doloribus tempora minima laudantium deserunt delectus incidunt. Cumque velit dignissimos libero odit odio quia reprehenderit voluptatibus! Esse, fuga nobis possimus quidem iste nisi. Cupiditate optio quaerat quidem quas earum officia doloremque cum iste ipsa, incidunt adipisci et sequi nostrum provident, praesentium ratione dolore nam, maiores ab eos veritatis quibusdam eaque! Minus cumque doloremque officia delectus molestias rem adipisci necessitatibus facere sit tempore. Magnam, itaque quam quidem laboriosam neque impedit.</p>
+      
     </div>
   );
 };
